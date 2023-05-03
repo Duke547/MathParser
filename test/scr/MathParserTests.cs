355fe57882c9dd8ac5776d98b3930655d1279901
Gage Duke <72324996+Duke547@users.noreply.github.com>
2023-05-03 08:30:31 -0400

    Refactor parser tests

--- test/scr/MathParserTests.cs
+++ test/scr/MathParserTests.cs
@@ -5,155 +5,72 @@ namespace MathParsing.Testing;
 
 internal class MathParserTests
 {
+    private static void TestValidExpression(string expression, decimal expectedResult)
+    {
+        Assert.Multiple(() =>
+        {
+            Assert.That(() => { var result = Parser.Parse(expression); }, Throws.Nothing, $"{expression}");
+            Assert.That(Parser.Parse(expression), Is.EqualTo(expectedResult), $"{expression}");
+        });
+    }
+
     [Test]
     public void Parse_Valid_Tests()
     {
-        var expressions = new string[]
-        {
-            "",
-            "1 + 2 + 3",
-            "1 + 2 * 3",
-            "3 * 2 + 1",
-            "1+2+3",
-            "1 - 2 * 3",
-            "2 × 3",
-            "2 ∙ 3",
-            "5 / 2",
-            "5 ÷ 2",
-            "5 % 4",
-            "3 * (2 + 1)",
-            "(3 * 2) + 1",
-            "(3 * 2) + (1 + 3)",
-            "()",
-            "(5)",
-            "((5))",
-        };
-
-        var expectedResults = new decimal[]
-        {
-             0.0m,
-             6.0m,
-             7.0m,
-             7.0m,
-             6.0m,
-            -5.0m,
-             6.0m,
-             6.0m,
-             2.5m,
-             2.5m,
-             1.0m,
-             9.0m,
-             7.0m,
-            10.0m,
-             0.0m,
-             5.0m,
-             5.0m,
-        };
-
         Assert.Multiple(() =>
         {
-            for (int i = 0; i < expressions.Length; i++)
-            {
-                var expression     = expressions[i];
-                var expectedResult = expectedResults[i];
-                
-                Assert.That(() => { var result = Parser.Parse(expression); }, Throws.Nothing, $"{expression}");
-                Assert.That(Parser.Parse(expression), Is.EqualTo(expectedResult), $"{expression}");
-            }
+            TestValidExpression("",                   0.0m);
+            TestValidExpression("1 + 2 + 3",          6.0m);
+            TestValidExpression("1 + 2 * 3",          7.0m);
+            TestValidExpression("3 * 2 + 1",          7.0m);
+            TestValidExpression("1+2+3",              6.0m);
+            TestValidExpression("1 - 2 * 3",         -5.0m);
+            TestValidExpression("2 × 3",              6.0m);
+            TestValidExpression("2 ∙ 3",              6.0m);
+            TestValidExpression("5 / 2",              2.5m);
+            TestValidExpression("5 ÷ 2",              2.5m);
+            TestValidExpression("5 % 4",              1.0m);
+            TestValidExpression("3 * (2 + 1)",        9.0m);
+            TestValidExpression("(3 * 2) + 1",        7.0m);
+            TestValidExpression("(3 * 2) + (1 + 3)", 10.0m);
+            TestValidExpression("()",                 0.0m);
+            TestValidExpression("(5)",                5.0m);
+            TestValidExpression("((5))",              5.0m);
         });
+    }
+
+    private static void TestInvalidExpression(string  expression,    Type expectedExceptionType, string expectedMessage,
+                                              string? expectedToken, int? expectedPosition)
+    {
+        if (expectedExceptionType == typeof(IndexedTokenException))
+        {
+            Assert.That(() => Parser.Parse(expression), Throws
+                .TypeOf<IndexedTokenException>()
+                .With.Message             .EqualTo(expectedMessage)
+                .And .Property("Token"   ).EqualTo(expectedToken)
+                .And .Property("Position").EqualTo(expectedPosition), expression);
+        }
+        else
+        {
+            Assert.That(() => Parser.Parse(expression), Throws.TypeOf<InvalidOperationException>()
+                .With.Message.EqualTo(expectedMessage), expression);
+        }
     }
 
     [Test]
     public void Parse_Invalid_Tests()
     {
-        var expressions = new[]
-        {
-            "1 + 1 &",
-            "1 + + 1",
-            "1 + 1 +",
-            "1 + 1 1",
-            "+ 1",
-            "(1 + 1",
-            "1 + 1)",
-            "(+ 1)",
-            "(1 +)",
-        };
-
-        var expectedExceptionTypes = new[]
-        {
-            typeof(IndexedTokenException),
-            typeof(IndexedTokenException),
-            typeof(IndexedTokenException),
-            typeof(IndexedTokenException),
-            typeof(IndexedTokenException),
-            typeof(InvalidOperationException),
-            typeof(InvalidOperationException),
-            typeof(IndexedTokenException),
-            typeof(IndexedTokenException),
-        };
-
-        var expectedMessages = new[]
-        {
-            "Unrecognized token '&' at position 6.",
-            "Unexpected token '+' at position 4.",
-            "Unexpected token '+' at position 6.",
-            "Unexpected token '1' at position 6.",
-            "Unexpected token '+' at position 0.",
-            "Missing ')'.",
-            "Missing '('.",
-            "Unexpected token '+' at position 1.",
-            "Unexpected token ')' at position 4.",
-        };
-
-        var expectedTokens = new[]
-        {
-            "&",
-            "+",
-            "+",
-            "1",
-            "+",
-            null,
-            null,
-            "+",
-            ")",
-        };
-
-        var expectedPositions = new int?[]
-        {
-            6,
-            4,
-            6,
-            6,
-            0,
-            null,
-            null,
-            1,
-            4,
-        };
-
         Assert.Multiple(() =>
         {
-            for (int i = 0; i < expressions.Length; i++)
-            {
-                var expression    = expressions           [i];
-                var exceptionType = expectedExceptionTypes[i];
-                var message       = expectedMessages      [i];
-                var token         = expectedTokens        [i];
-                var position      = expectedPositions     [i];
-
-                if (exceptionType == typeof(IndexedTokenException))
-                {
-                    Assert.That(() => Parser.Parse(expression), Throws.TypeOf<IndexedTokenException>()
-                        .With.Message             .EqualTo(message)
-                        .And .Property("Token"   ).EqualTo(token)
-                        .And .Property("Position").EqualTo(position), expression);
-                }
-                else
-                {
-                    Assert.That(() => Parser.Parse(expression), Throws.TypeOf<InvalidOperationException>()
-                        .With.Message.EqualTo(message), expression);
-                }
-            }
+            TestInvalidExpression("1 + 1 &", typeof(IndexedTokenException),     "Unrecognized token '&' at position 6.", "&",  6   );
+            TestInvalidExpression("1 + + 1", typeof(IndexedTokenException),     "Unexpected token '+' at position 4.",   "+",  4   );
+            TestInvalidExpression("1 + 1 +", typeof(IndexedTokenException),     "Unexpected token '+' at position 6.",   "+",  6   );
+            TestInvalidExpression("1 + 1 1", typeof(IndexedTokenException),     "Unexpected token '1' at position 6.",   "1",  6   );
+            TestInvalidExpression("+ 1",     typeof(IndexedTokenException),     "Unexpected token '+' at position 0.",   "+",  0   );
+            TestInvalidExpression("(1 + 1",  typeof(InvalidOperationException), "Missing ')'.",                          null, null);
+            TestInvalidExpression("1 + 1)",  typeof(InvalidOperationException), "Missing '('.",                          null, null);
+            TestInvalidExpression("(+ 1)",   typeof(IndexedTokenException),     "Unexpected token '+' at position 1.",   "+",  1   );
+            TestInvalidExpression("(1 +)",   typeof(IndexedTokenException),     "Unexpected token ')' at position 4.",   ")",  4   );
         });
     }
 
